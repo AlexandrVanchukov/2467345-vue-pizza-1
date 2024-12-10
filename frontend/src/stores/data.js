68061836961146d85d9b9e619f36365651f70787
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-12-10 15:44:49 +0300

    5.3 Добавление геттеров хранилища

--- frontend/src/stores/data.js
+++ frontend/src/stores/data.js
@@ -1,12 +1,29 @@
 import { defineStore } from "pinia";
 
+import doughJSON from "@/mocks/dough.json";
+import ingredientsJSON from "@/mocks/ingredients.json";
+import saucesJSON from "@/mocks/sauces.json";
+import sizesJSON from "@/mocks/sizes.json";
+import miscJSON from "@/mocks/misc.json";
+
+// import { translateNameToEng } from "../helpers/translate-name";
+
+// function translateNames(dataJSON) {
+//   const updatedJSON = dataJSON.map((item) => ({
+//     ...item,
+//     name_eng: translateNameToEng(item.name),
+//   }));
+
+//   return updatedJSON;
+// }
+
 export const useDataStore = defineStore("data", {
   state: () => ({
-    dough: [],
-    ingredients: [],
-    misc: [],
-    sauce: [],
-    sizes: [],
+    dough: doughJSON,
+    ingredients: ingredientsJSON,
+    sauce: saucesJSON,
+    sizes: sizesJSON,
+    misc: miscJSON,
   }),
   getters: {},
   actions: {},
